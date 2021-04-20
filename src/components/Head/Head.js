da6321704e9a6630f7c385cea53bea74589de054
TanzeelTasleem <tanzeeltasleem648@gmail.com>
2021-04-21 03:15:41 +0500

    head updated

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 function Head({id}) {
-  console.log("********************* id from head component")
+  console.log("********************* id from head component",id)
 
   return (
     <Helmet>
      {
         typeof window !== `undefined` &&
-        <>
         <script type="text/javascript" async src={`https://test.oppwa.com/v1/paymentWidgets.js?checkoutId=${id}`}>
         </script>
-        </>
      }
     </Helmet>
   );
