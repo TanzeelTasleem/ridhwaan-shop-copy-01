61fce2138c6f12282b6f5a916882d87112456dd6
TanzeelTasleem <tanzeeltasleem648@gmail.com>
2021-04-21 06:05:37 +0500

    some changes

--- src/pages/paymentStatus.js
+++ src/pages/paymentStatus.js
@@ -6,41 +6,41 @@ export const PaymentStatus = (props) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState({ state: false, msg: "" });
 
-  const checkPaymentStatus = async () => {
-    const paymentSessionId = new URLSearchParams(window.location.search).get(
-      "paymentSessionId"
-    );
+//   const checkPaymentStatus = async () => {
+//     const paymentSessionId = new URLSearchParams(window.location.search).get(
+//       "paymentSessionId"
+//     );
 
-    const resourcePath = new URLSearchParams(window.location.search).get(
-      "resourcePath"
-    );
+//     const resourcePath = new URLSearchParams(window.location.search).get(
+//       "resourcePath"
+//     );
 
-    const body = {
-      paymentSessionId,
-      resourcePath,
-    };
+//     const body = {
+//       paymentSessionId,
+//       resourcePath,
+//     };
 
-    try {
-      const response = await axios.post(
-        "https://ridhwaan-shop-03fa0.netlify.app/.netlify/functions/paymentConfirm",
-        body
-      );
-      setLoading(false);
-      window.location.href = await response.data.returnUrl;
-    } catch (err) {
-      console.log(err);
-      setError({
-        state: true,
-        msg: err.msg,
-      });
-    }
-  };
+//     try {
+//       const response = await axios.post(
+//         "https://ridhwaan-shop-03fa0.netlify.app/.netlify/functions/paymentConfirm",
+//         body
+//       );
+//       setLoading(false);
+//       window.location.href = await response.data.returnUrl;
+//     } catch (err) {
+//       console.log(err);
+//       setError({
+//         state: true,
+//         msg: err.msg,
+//       });
+//     }
+//   };
 
-  useEffect(() => {
-    (async () => {
-      await checkPaymentStatus();
-    })();
-  }, []);
+//   useEffect(() => {
+//     (async () => {
+//       await checkPaymentStatus();
+//     })();
+//   }, []);
 
   return (
     <Layout {...props}>
