2328b933d7ef42de65358b4b32abbef076c38fe7
TanzeelTasleem <tanzeeltasleem648@gmail.com>
2021-04-26 17:31:03 +0500

    functions added

---
+++ functions/paymentMethod/index.js
@@ -0,0 +1,41 @@
+const axios = require("axios")
+require("dotenv").config()
+
+exports.handler = async function (event) {
+  console.log(event)
+
+  const request = JSON.parse(event.body)
+  console.log("Request = ", request.publicToken)
+
+  console.log("site url ==", process.env.SITE_URL)
+
+  const response = await axios.get(
+    `https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.publicToken}`
+  )
+
+  console.log("RESPONSE STATUS =", response.status)
+  // Return a 404 if the request is not from Snipcart
+  if (!response.status == 200)
+    return {
+      statusCode: 404,
+      body: "public token validation failed",
+    }
+
+  // Create a payment method list
+
+  let paymentMethodList = [
+    {
+      name:"Peach Payments",
+      id:"peach_payment",
+      checkoutUrl:`${process.env.SITE_URL}/checkoutForm/?publicToken=${request.publicToken}`,
+    },
+  ]
+
+  console.log(paymentMethodList)
+
+  // Return successful status code and available payment methods
+  return {
+    statusCode: 200,
+    body: JSON.stringify(paymentMethodList),
+  }
+}
---
+++ functions/shippingRates/index.js
@@ -0,0 +1,83 @@
+const axios = require("axios")
+const uuid = require("uuid")
+require("dotenv").config()
+
+exports.handler = async event => {
+
+  const data = JSON.parse(event.body)
+
+  if ((data.eventName = "shippingrates.fetch")) {
+    
+
+    const {content: { shippingAddress }} = data
+
+    const {content: {items}} = data
+
+    const body = {
+      WS_Key: `${process.env.SHIP2_API_TOKEN}`,
+      Sender: {
+        Address1: "203 Montevideo, 4 Ninth Street",
+        City: "Johannesburg",
+        Country: "ZA",
+        IsResidential: true,
+        PostalCode: "2193",
+      },
+      Recipient: {
+        Address1: `${shippingAddress.fullAddress}`,
+        City: `${shippingAddress.city}`,
+        CompanyName: "Ridhwaan",
+        Country: `${shippingAddress.country}`,
+        IsResidential: false,
+        PostalCode: `${shippingAddress.postalCode}`,
+      },
+      Packages: [
+        {
+          Weight: items[0].totalWeight / 454,
+        },
+      ],
+    }
+
+    try {
+
+      const result = await axios.post(`${process.env.SHIPPING_RATE_URL}`, body)
+
+      const couriersData = result.data[0]["Services"]
+
+      const couriersList = couriersData.map(obj => {
+        return {
+          cost: obj.CustomerPrice.Total,
+          description: obj.Service.Name,
+          userDefinedId: uuid.v4(),
+        }
+      })
+
+      return {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          rates: couriersList,
+        })
+      }
+
+    } catch (error) {
+      
+      return {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          errors: [{
+            key: error.response.data.Message,
+            message: error.response.data.ExceptionMessage
+          }],
+        }),
+      
+      }
+
+    }
+
+  }
+}
